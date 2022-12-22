5ed805b18186238cb152b460cc12390c3cdfdcbd
Vlad20RD <vladcska35topppchik@gmail.com>
2022-12-22 12:35:44 +0300

    add WDM

--- src/main/java/helper/BaseHelper.java
+++ src/main/java/helper/BaseHelper.java
@@ -1,11 +1,16 @@
 package helper;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeEach;
 
 public class BaseHelper {
     @BeforeEach
     public void setUpDriver() {
+        WebDriverManager.chromedriver().setup();
         Configuration.browserSize = "1920x1080";
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 }
